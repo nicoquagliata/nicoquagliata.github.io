a27911f3cea2fd2ba4ac28f99554531a16f44262
Nicolas <nicoquagliata@gmail.com>
2022-12-29 11:24:24 -0300

    Update VRButton.js

--- VRButton.js
+++ VRButton.js
@@ -15,7 +15,7 @@ class VRButton{
             button.style.height = '40px'
             document.body.appendChild( button )
 
-            navigator.xr.isSessionSupported('immersive-vrX').then((supported)=>{
+            navigator.xr.isSessionSupported('immersive-vr').then((supported)=>{
                 supported ? this.showEnterVR(button) : this.showWebXRNotFound(button)
             })
 		} else {
@@ -25,7 +25,13 @@ class VRButton{
     }
 
 	showEnterVR( button ) {
-    
+        this.stylizeElement(button, true, 30, true)
+        button.style.display = ''
+        button.style.right = '20px'
+        button.style.width = '80px'
+        button.style.cursor = 'pointer'
+        button.innerHTML = '<i class="fas fa-vr-cardboard"></i>'
+
     }
 
     disableButton( button ) {
