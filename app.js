7f9cde68dcb9247404e6c27af629f932e23ff962
Nicolas <nicoquagliata@gmail.com>
2022-12-29 10:40:53 -0300

    FIX

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ class App{
         this.radius = 0.08
         this.room = new THREE.LineSegments(
             new BoxLineGeometry(6,6,6,10,10,10),
-            new LineBasicMaterial( {color: 0x808080})
+            new THREE.LineBasicMaterial( {color: 0x808080})
         )
         this.room.geometry.translate(0,3,0)
         this.scene.add(this.room)
